0946584049e56a0335b48f71b3ad3f18b4b9bbc0
KyuJinLee0911 <rbwls1001@naver.com>
2024-04-23 16:03:20 +0900

    캐릭터 컨트롤 움직임 부분 완성
    - 애니메이션을 좀 더 자연스럽게 수정
    - 캐릭터가 움직이지 않을때는 카메라가 바라보는 방향으로 회전하지 않고, 움직일 때만 카메라가 바라보는 방향으로 회전하여 멈춰있을때는 캐릭터와 주변을 살펴보기 용이하도록 수정

--- ProjectPR/Assets/Scripts/Controls/PlayerController.cs
+++ ProjectPR/Assets/Scripts/Controls/PlayerController.cs
@@ -26,6 +26,9 @@ public class PlayerController : MonoBehaviour
 
     float moveSpeed = 0.15f;
 
+    Quaternion targetRotation;
+    bool isMoving = false;
+
     private void Awake()
     {
         cameraTransform = Camera.main.transform;
@@ -39,16 +42,19 @@ public class PlayerController : MonoBehaviour
     private void OnEnable()
     {
         moveAction.Enable();
+        moveAction.performed += MoveCheck; // 움직이고 있을때 isMoving을 true로 바꿔줌
+        moveAction.canceled += MoveCheck; // 움직임이 끝날 때 isMoving을 false로 바꿔줌
+        
         jumpAction.Enable();
-
         jumpAction.performed += JumpPerformed;
-
+        targetRotation = transform.rotation;
     }
 
     private void FixedUpdate()
     {
         Vector2 keyboard_vector = moveAction.ReadValue<Vector2>();
-        MOVE(keyboard_vector.x, keyboard_vector.y);
+        if(isMoving)
+            MOVE(keyboard_vector.x, keyboard_vector.y);
 
         // Raycast를 통해 땅에 닿았는지 체크.
         // Raycast의 시작지점을 0.1만큼 띄운 이유는 발 밑에서 바로 레이를 나가게 하면 가끔 울퉁불퉁한 지형에서 땅을 밟고 있음에도 밟고있지 않다고 판정이 날 때가 있었음
@@ -63,21 +69,39 @@ public class PlayerController : MonoBehaviour
         jumpAction.performed -= JumpPerformed;
     }
 
+    void MoveCheck(InputAction.CallbackContext context)
+    {
+        float _x, _z;
+        Vector2 keyboardVector = context.ReadValue<Vector2>();
+        _x = keyboardVector.x;
+        _z = keyboardVector.y;
+
+        // 움직이는지 아닌지에 따라 isMoving 변수 결정. 그 뒤 플레이어 애니메이션 바꿈
+        isMoving = _x != 0 || _z != 0 ? true : false;
+        playerAnimator.SetBool("isMoving", isMoving);
+    }
+
+    
 
     void MOVE(float _x, float _z)
     {
-        bool isMoving = _x != 0 || _z != 0 ? true : false;
-        playerAnimator.SetBool("isMoving", isMoving);
+        // 앞으로 가는지 뒤로 가는지에 따라 애니메이션 결정
         bool isForward = _z >= 0 ? true : false;
-        playerAnimator.SetBool("isForward", isForward);
+        playerAnimator.SetBool("isForward", isForward);  
 
+        // 왜 제곱수로 하는지?
+        // => 대각선으로 움직일때 45도의 각도로 회전시켜주기 위해
         float square = _x >= 0 ? _x * _x : -1 * (_x * _x);
-        square = isForward ? square : -square;
-        Quaternion targetRotation = Quaternion.Euler(0, transform.rotation.y + square * 90 + cameraTransform.rotation.eulerAngles.y, 0);
-        Debug.Log(cameraTransform.rotation.y);
+        // 뒤로 갈때는 좌우 바라보는 방향을 반대로
+        square = _z >= 0 ? square : -square;
 
-        transform.position += transform.forward * _z * moveSpeed + transform.right * _x * moveSpeed;
+        // 따로 x축 입력이 없으면 카메라가 바라보는 방향으로 회전, x축 입력이 있으면 카메라가 바라보는 방향 + 입력만큼의 각도로 회전
+        targetRotation = Quaternion.Euler(0, transform.rotation.y + square * 90 + cameraTransform.rotation.eulerAngles.y, 0); 
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 0.2f);
+
+        // y축 입력이 양수일 경우 캐릭터가 바라보는 방향으로 진행, 음수일 경우 반대 방향으로 진행
+        Vector3 moveDir = _z >=0 ? transform.forward : transform.forward * -1;
+        transform.position = transform.position + moveDir * moveSpeed;
     }
 
     void JumpPerformed(InputAction.CallbackContext context)
